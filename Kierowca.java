8aa4d9353546abe0aa5de662de27b7d0edab23f8
Lukasz2004 <lukaszczerwinski2004@gmail.com>
2024-05-26 18:20:18 +0200

    Dodano podstawy metod Maina

--- Kierowca.java
+++ Kierowca.java
@@ -5,4 +5,5 @@ public class Kierowca extends Personel {
     public double agresywnosc;
     public double adaptacjaPogoda;
     public double ekonomicznoscJazdy;
+    public double odlegloscOdPoprzednika;
 }
--- Main.java
+++ Main.java
@@ -1,32 +1,75 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
-    private int liczbaOkrazenNaTor = 50;
-    private ArrayList<Kierowca> listaKierowcow = new ArrayList<Kierowca>();
-    public void main(String[] args){
-        System.out.println("Hello World!");
-        /*
+    private static final int liczbaOkrazenNaTor = 50;
+    private static ArrayList<Kierowca> listaKierowcow = new ArrayList<Kierowca>();
+    private static ArrayList<String> listaTorow = new ArrayList<String>();
+    public static void main(String[] args){
         wczytajDane();
-        for(int nrWyscigu=1; nrWyscigu<=LiczbaTorow; nrWyscigu++)
+        for(int nrWyscigu=1; nrWyscigu<=listaTorow.size(); nrWyscigu++)
         {
-            uruchomWyscig(Tory[nrWyscigu-1]);
+            uruchomWyscig(listaTorow.get(nrWyscigu - 1));
             zapiszWyniki();
         }
         pokazWyniki();
         zapiszWyniki();
-        */
     }
-    /*
-    private void uruchomWyscig(Tor tor){
+    private static void uruchomWyscig(String tor){
         for(int okrazenie=0; okrazenie<liczbaOkrazenNaTor; okrazenie++)
         {
             for(int i=0; i<listaKierowcow.size();i++)
             {
-                przejazdKierowcy(listaKierowcow[i]);
+                przejazdKierowcy(listaKierowcow.get(i));
+            }
+            for(int i=1; i<listaKierowcow.size();i++)
+            {
+                wyprzedzanie(i);
             }
         }
     }
-     */
+    private static void przejazdKierowcy(Kierowca kierowca)
+    {
+        double czasPrzejazdu = 1;
+        kierowca.odlegloscOdPoprzednika = Math.min(kierowca.odlegloscOdPoprzednika,kierowca.odlegloscOdPoprzednika-czasPrzejazdu);
+    }
+    private static void wyprzedzanie(int pozKierowcy)
+    {
+        Kierowca kierowca1 = listaKierowcow.get(pozKierowcy-1);
+        Kierowca kierowca2 = listaKierowcow.get(pozKierowcy);
+    }
+    private static void zapiszWyniki() {
+
+    }
+
+    private static void pokazWyniki() {
+
+    }
 
 
+    private static List<String> liniaCSVnaDane(String linia) {
+        List<String> wartosci = new ArrayList<String>();
+        try (Scanner scanner = new Scanner(linia)) {
+            scanner.useDelimiter(",");
+            while (scanner.hasNext()) {
+                wartosci.add(scanner.next());
+            }
+        }
+        return wartosci;
+    }
+    private static void wczytajDane(){
+        String sciezkaDaneKierowcow = "DaneStartowe/kierowcy.csv";
+
+        List<List<String>> linieTekstu = new ArrayList<>();
+        try (Scanner scannerKierowcow = new Scanner(new File(sciezkaDaneKierowcow))) {
+            while (scannerKierowcow.hasNextLine()) {
+                linieTekstu.add(liniaCSVnaDane(scannerKierowcow.nextLine()));
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
