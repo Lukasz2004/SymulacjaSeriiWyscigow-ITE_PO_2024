a8474d2643d788d98c984b549f3766db227fa202
Lukasz2004 <lukaszczerwinski2004@gmail.com>
2024-06-16 20:21:19 +0200

    Finalna dokumentacja

--- Ulepszenie.java
+++ Ulepszenie.java
@@ -1,4 +1,4 @@
-/** Interface zawiera metode wykorzystywana do ulepszen obiektow
+/** Interface zawiera metode wykorzystywana do ulepszen obiektow pomiedzy wyscigami. Wywolywany jest przez {@link Main#ulepszenia()}.
  * Korzystaja z niego klasy::
  *       <ul>
  *             <li>{@link Kierowca}</li>
@@ -7,7 +7,7 @@
  *      </ul>
  */
 public interface Ulepszenie {
-    /** Metoda ulepsza jedna statystyke o podana wartosc
+    /** Metoda ulepsza jedna statystyke z dostepnych o podana wartosc
      * @param wartosc double zawieracy wartosc ulepszenia
      */
     public void ulepszStatystyki(double wartosc);
